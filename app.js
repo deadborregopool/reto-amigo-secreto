8c3183ac9f9b84e8d7724ce6f2789e16b20ea0af
Luis Gutierrez <fabrizzio150@gmail.com>
2025-01-31 00:47:34 -0500

    Lista Creada y funcion AgregarAmigo() tambien con validacion

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+function agregarAmigo () {
+    let entrada= document.getElementById("amigo")
+    let validarAmigo = entrada.value.trim();
+
+    if(validarAmigo===""){
+        alert("Por favor, inserte un nombre")
+    }else{
+        amigos.push(validarAmigo);
+        entrada.value="";
+    }
+    
+}
